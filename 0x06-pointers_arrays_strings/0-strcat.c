6ae79182cce9362afb6a54af2b7dee5f90d8d67b
Daewonkarma12 <daewonkarma@gmail.com>
2022-09-22 17:43:43 -0700

    catt

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- *_strcat - concatenates the string pointed to by @src to
+ *_strcat - concatenates  the string pointed to by @src to
  * the end of the string pointed to by @dest
  *@dest: String that will be appended
  *@src: String to be concatenated upon
@@ -9,12 +10,13 @@
  */
 
 char *_strcat(char *dest, char *src)
+
 {
 
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
-	        dest_len++;
+		dest_len++;
 
 	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
