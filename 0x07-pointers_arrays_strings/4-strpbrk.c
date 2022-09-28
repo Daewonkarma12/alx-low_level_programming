55d312e3ac1cee188d594653bb4fa2c9c4d330cc
Daewonkarma12 <daewonkarma@gmail.com>
2022-09-27 20:14:53 -0700

    new

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,7 +3,6 @@
 /**
  * _strpbrk - searches a string for any of a set of bytes
  * @s: string to check
-
  * @accept: string to check against
  *
  * Return: pointer to byte in s that matches or NULL if no match
